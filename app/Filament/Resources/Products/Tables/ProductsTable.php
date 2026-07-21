907b7a73b694d5e727d099b00a792fab1225fafc
Kawsar Ahmad <kawsarmojomdar@gmail.com>
2026-07-21 22:44:42 +0600

    ok

--- app/Filament/Resources/Products/Tables/ProductsTable.php
+++ app/Filament/Resources/Products/Tables/ProductsTable.php
@@ -86,6 +86,7 @@ class ProductsTable
                     ->sortable()
                     ->toggleable(isToggledHiddenByDefault: true),
             ])
+            ->defaultSort('created_at', 'desc')
             ->filters([
                 TrashedFilter::make(),
             ])
